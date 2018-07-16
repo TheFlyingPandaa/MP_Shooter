afe181ad762de530d82724deb13bd3f84d34b10c
Unknown <hampuz.aronsson@gmail.com>
2018-07-16 21:31:31 +0200

    Added advanced sessions plugin

--- Source/MPShooter/MPShooter.Build.cs
+++ Source/MPShooter/MPShooter.Build.cs
@@ -6,12 +6,17 @@ public class MPShooter : ModuleRules
 {
 	public MPShooter(ReadOnlyTargetRules Target) : base(Target)
 	{
-        bFasterWithoutUnity = true;
-		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+        //bEnforceIWYU = true;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "OnlineSubsystem", "OnlineSubsystemUtils", "Steamworks" });
+        PublicDefinitions.Add("WITH_ADVANCED_SESSIONS=1");
 
+        PrivateIncludePaths.AddRange(new string[] { "AdvancedSessions/Private"/*, "OnlineSubsystemSteam/Private"*/ });
+        PublicIncludePaths.AddRange(new string[] { "AdvancedSessions/Public" });
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "OnlineSubsystem", "CoreUObject", "OnlineSubsystemUtils", "Networking", "Sockets","Voice", "OnlineSubsystemSteam"});
+        PrivateDependencyModuleNames.AddRange(new string[] { "OnlineSubsystem", "Sockets", "Networking", "OnlineSubsystemUtils","Voice", "Steamworks","OnlineSubsystemSteam"});
         PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
 
     }
 }
